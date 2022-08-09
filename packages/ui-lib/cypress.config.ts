e6131a4b3e94800645e546672fbfd9aefe9058d2
Nuno Gonçalves <ngocalves@gmail.com>
2022-08-10 01:12:40 +0200

    ci: Config github action tests with vitest and cypress on ui-lib when PR

--- packages/ui-lib/cypress.config.ts
+++ packages/ui-lib/cypress.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: false,
   projectId: '6zegkn',
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 
